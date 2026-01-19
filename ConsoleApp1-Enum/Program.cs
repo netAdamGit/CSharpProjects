350d2b93fe386543cf3e4ec8ff6fe6f8c6433160
netAdamGit <105038555+netAdamGit@users.noreply.github.com>
2026-01-19 08:24:26 +0300

    enum ile ilgili iki uygulama örneği

---
+++ ConsoleApp1-Enum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace EnumOrnek
+{
+    // Enum tanımı
+    public enum Gunler
+    {
+        Pazartesi,
+        Sali,
+        Carsamba,
+        Persembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Enum kullanım örneği
+            Gunler bugun = Gunler.Cuma;
+
+            Console.WriteLine($"Bugün: {bugun}");
+
+            // Switch-case ile kullanım
+            switch (bugun)
+            {
+                case Gunler.Pazartesi:
+                    Console.WriteLine("Haftanın ilk günü, enerjiyle başla!");
+                    break;
+                case Gunler.Cuma:
+                    Console.WriteLine("Hafta sonu yaklaşıyor, biraz daha sabır!");
+                    break;
+                case Gunler.Pazar:
+                    Console.WriteLine("Dinlenme günü, yeni haftaya hazırlan.");
+                    break;
+                default:
+                    Console.WriteLine("Normal bir gün.");
+                    break;
+            }
+        }
+    }
+}
+
---
+++ ConsoleApp1-enumMevsimler/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+    class MevsimBulucu  
+    {  
+    enum Mevsim { Ilkbahar,Yaz,Sonbahar,Kis}
+        static void Main()
+        {
+        Console.Write("1-12 arasında bir sayı giriniz! :");
+        int ay = int.Parse(Console.ReadLine());
+        Mevsim mevsim;
+        if (ay >=1 && ay <=3) mevsim = Mevsim.Kis;
+        else if (ay >=4 && ay <=6 ) mevsim = Mevsim.Ilkbahar;
+        else if (ay >=7 && ay <= 9) mevsim = Mevsim.Yaz;
+        else mevsim = Mevsim.Sonbahar;
+        Console.WriteLine("Mevsim: "+mevsim);  
+        }
+    }
+
