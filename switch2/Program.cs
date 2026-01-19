85ef7ab38207639f7cb6e35897ad8f2751b8972c
netAdamGit <105038555+netAdamGit@users.noreply.github.com>
2026-01-19 08:30:05 +0300

    switch koşul ifadesi
    switch koşul ifadesi örneği

---
+++ switch2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch2
+{
+    internal class Program
+    {
+        
+        static void Main()
+        {
+            Console.Write("Bir gün sayısı gir (1-7) :");
+            string  secim=Console.ReadLine();
+            if (int.TryParse(secim, out int gunNo))
+            {
+                switch (gunNo)
+                {
+                    case 1:
+                        Console.WriteLine("Pazartesi");
+                        break;
+                    case 2:
+                        Console.WriteLine("Salı");
+                        break;
+                    case 3:
+                        Console.WriteLine("Çarşamba");
+                        break;
+                    case 4:
+                        Console.WriteLine("Perşembe");
+                        break;
+                    case 5:
+                        Console.WriteLine("Cuma");
+                        break;
+                    case 6:
+                        Console.WriteLine("Cumartesi");
+                        break;
+                    case 7:
+                        Console.WriteLine("Pazar");
+                        break;
+                    default:
+                        Console.WriteLine("Geçersiz gün numarası! Lütfen 1-7 arasında bir sayı giriniz.");
+                        break;
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("Hatalı giriş! Lütfen sadece sayı giriniz.");
+            }
+
+
+
+        }
+    }
+}
